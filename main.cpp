b4b8395f2fbc888e3922aa47f5ecfb148d90462c
m <mychal.edgar12@gmail.com>
2025-03-30 14:26:39 +0700

    main.cpp

--- main.cpp
+++
@@ -1,3 +0,0 @@
-#include<iostream>
-#include<limits>
-using namespace std;struct a{string b;double c;};int main(){int d;for(;;){cout<<"Enter a rebirth amount: ";cin>>d;if(cin.fail()||d<1)cin.clear(),cin.ignore(numeric_limits<streamsize>::max(),'\n'),cout<<"Invalid input! Please enter an integer greater than or equal to 1.\n";else break;}a e[]={{"Tiny",.025},{"Punching",.05},{"Large",.075},{"Golden",.2},{"Frozen",.375},{"Mystic",.75},{"Inferno",1.125},{"Legends",2.5},{"Muscle King",12.5},{"Ancient Jungle",16.25}};for(auto f:e)cout<<f.b<<" rock: "<<(d+20)*f.c<<endl;}

eec5b8ca2588a4bcd4cf1a02bd30e409fc267b72
m <mychal.edgar12@gmail.com>
2025-03-30 15:01:03 +0700

    calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -7,23 +7,27 @@ struct rock {
 };
 
 int main(int argc, char* argv[]) {
-	if (argc != 3)
+	if (argc != 3) {
 		std::cout << "Error: Missing arguments. Use '-r <number>' or '--rebirths <number>'.";
-		return 0;
+		return 1;
+	}
 
-	if (std::string(argv[1]) != "-r" && std::string(argv[1]) != "--rebirths")
+	if (std::string(argv[1]) != "-r" && std::string(argv[1]) != "--rebirths") {
 		std::cout << "Error: Invalid option. Use '-r' or '--rebirths'.";
-		return 0;
+		return 1;
+	}
 
 	int rebirthAmount;
 	try {
 		rebirthAmount = std::stoi(argv[2]);
-		if (rebirthAmount < 1)
+		if (rebirthAmount < 1) {
 			std::cout << "Error: Rebirth amount must be at least 1.";
-			return 0;
-	} catch (const std::exception&)
+			return 1;
+		}
+	} catch (const std::exception&) {
 		std::cout << "Error: Invalid number format. Please enter a valid integer.";
-		return 0;
+		return 1;
+	}
 
 	rock rocks[] = {
 		{ "Tiny", .025 }, { "Punching", .05 }, { "Large", .075 },
